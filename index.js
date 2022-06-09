d0e4f608c97b07db837e71d521afb78b37362f46
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-06-09 13:39:18 +0100

    deployed circleci

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/deployed', (req, res) => {
+  res.send('deployed page');
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
